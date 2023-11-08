a2bc742a774b5afb23019199e9e5c236ad23b965
Kilian Arjona <23211440+karjona@users.noreply.github.com>
2023-11-08 20:50:36 +0100

    Improve movement arrows

--- src/utilities/moves.lua
+++ src/utilities/moves.lua
@@ -41,43 +41,31 @@ function draw_arrow(ship, type, speed, orientation)
   local turn_offset = 0
 
   if type == "bank" then
-    if orientation == "left" then
-      angle = 45 / 360
-      turn_offset = 5
-    else
-      angle = 315 / 360
-      turn_offset = -6
-    end
-
-    for i = 1, speed do
-      rspr(ship.x, ship.y - i * 8, angle, 4.5, .5, 1, false, 1)
-    end
-    rspr(ship.x + turn_offset, ship.y - 4 - speed * 8, angle, 2.5, .5, 2, false, 1)
   end
 
   if type == "turn" then
-    for i = 1, speed do
+    for i = 1, speed - 1 do
       spr(81, ship.x - 4, ship.y - 4 - i * 8)
     end
     if orientation == "left" then
       angle = 90 / 360
-      turn_offset = 7
+      turn_offset = 8
     else
       angle = 270 / 360
       turn_offset = -8
     end
     if orientation == "left" then
-      spr(83, ship.x - 4, ship.y - 8 - speed * 8, 1, 1)
+      spr(83, ship.x - 4, ship.y - 4 - speed * 8, 1, 1)
     else
-      spr(83, ship.x - 4, ship.y - 8 - speed * 8, 1, 1, true)
+      spr(83, ship.x - 4, ship.y - 4 - speed * 8, 1, 1, true)
     end
-    rspr(ship.x + turn_offset, ship.y - speed * 8 - 4, angle, 2.5, .5, 1.4, false, 1)
+    rspr(ship.x + turn_offset, ship.y - speed * 8, angle, 2.5, .5, 1.4, false, 1)
   end
 
   if type == "straight" then
-    for i = 1, speed do
+    for i = 1, speed - 1 do
       spr(81, ship.x - 4, ship.y - 4 - i * 8)
     end
-    spr(82, ship.x - 4, ship.y - 12 - speed * 8)
+    spr(82, ship.x - 4, ship.y - 4 - speed * 8)
   end
 end
